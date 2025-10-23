9ebb4a6d0ff0ca22da4e8ae43db9a2746db2a587
simonnien <106265667+simonnien@users.noreply.github.com>
2025-10-23 21:32:44 +0800

    Add files via upload

---
+++ sample3.java
@@ -0,0 +1,10 @@
+
+public class sample3
+{
+    public static void main(String[] args)
+    {
+        System.out.println('A');
+        System.out.println("歡迎使用java!");
+        System.out.println(123);
+    }
+}
